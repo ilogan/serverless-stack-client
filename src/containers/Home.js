ea134e9e99b330773e0daf09b8f304f50e5f25c2
Ian Logan <itlogan9@gmail.com>
2019-10-03 13:23:29 -0500

    Dynamically render home page using authenitcation

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { PageHeader, ListGroup } from "react-bootstrap";
 import "./Home.css";
 
-export default function Home() {
-  return (
-    <div className="Home">
+export default function Home({ isAuthenticated }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [notes, setNotes] = useState([]);
+
+  const renderNotesList = notes => {
+    return null;
+  };
+
+  const renderLander = () => {
+    return (
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
       </div>
+    );
+  };
+
+  const renderNotes = () => {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  };
+
+  return (
+    <div className="Home">
+      {isAuthenticated ? renderNotes() : renderLander()}
     </div>
   );
 }
